a472316b93bb0ebedc37152b78db00a9c301d91e
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-03-30 17:16:55 +0000

    Add Vuetify to the project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/static/openhab-scheduler/" : "/"
+    process.env.NODE_ENV === "production" ? "/static/openhab-scheduler/" : "/",
+  transpileDependencies: ["vuetify"]
 };

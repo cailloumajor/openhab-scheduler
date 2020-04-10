f56571f55f0531be559cad43d0846b37b67507c0
Arnaud Rocher <arnaud.roche3@gmail.com>
2020-04-10 15:00:20 +0000

    Add ability to debug in Firefox

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== "production") {
+      config.devtool = "source-map"
+    }
+  },
   publicPath:
     process.env.NODE_ENV === "production" ? "/static/openhab-scheduler/" : "/",
   transpileDependencies: ["vuetify"]
